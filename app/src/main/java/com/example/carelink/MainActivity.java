4c9a12bf6fa99c1aab7ba96cf6bd6a9f79e57f15
nextin <nextin.pvt@gmail.com>
2024-08-30 18:52:39 +0530

    feagment implement

--- app/src/main/java/com/example/carelink/MainActivity.java
+++ app/src/main/java/com/example/carelink/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.carelink;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.carelink.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new fragment_home());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new fragment_home());
+                    break;
+                case R.id.shorts:
+                    replaceFragment(new fragment_shorts());
+                    break;
+                case R.id.subscriptions:
+                    replaceFragment(new fragment_subscription());
+                    break;
+                case R.id.library:
+                    replaceFragment(new fragment_library());
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
